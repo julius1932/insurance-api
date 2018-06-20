bc7b6bdc870abcdaa30963948dd661cea6bc3bf6
julius1932 <30441147+julius1932@users.noreply.github.com>
2018-06-20 12:48:00 +0200

    Add files via upload

--- csvToMongo.js
+++ csvToMongo.js
@@ -1,18 +1,32 @@
 const csv=require('csvtojson');
-const csvFilePath='small.csv';
 
 const MongoClient = require('mongodb').MongoClient;
-const urlll = "mongodb://localhost:27017/";
+//const urlll = "mongodb://localhost:27017/";
+const urlll = "mongodb://junta:rootjunta123@ds163850.mlab.com:63850/insurance_db";
 
-csv()
-.fromFile(csvFilePath)
-.then((jsonObj)=>{
-    console.log(jsonObj);
-    MongoClient.connect(urlll, function(err, db) {
-       if (err) {isfound=false; return;}
-       var dbo = db.db("insurance_db");
-       dbo.collection("insurance").insert(jsonObj, function(er, res) {
-             console.log(`1 document inserted. ${jsonObj.length} items scraped and saved so far`);
-        });
-      })
-})
+var dFiles=[
+             'data/individual_issuer_county_report_2014_2018_05_12.csv',
+             'data/individual_issuer_county_report_2015_2018_05_12.csv',
+             'data/individual_issuer_county_report_2016_2018_05_12.csv',
+             'data/individual_issuer_county_report_2017_2018_05_12.csv',
+             'data/individual_issuer_county_report_2018_2018_06_01.csv',
+             'data/small_group_issuer_county_report_2015_2018_05_12.csv',
+             'data/small_group_issuer_county_report_2016_2018_05_12.csv',
+             'data/small_group_issuer_county_report_2017_2018_05_12.csv'
+         ];
+    dFiles.forEach(function(csvFilePath){
+      csv()
+      .fromFile(csvFilePath)
+      .then((jsonObj)=>{
+          console.log(jsonObj.length);
+          console.log(csvFilePath);
+          MongoClient.connect(urlll, function(err, db) {
+             if (err) {isfound=false; return;}
+             var dbo = db.db("insurance_db");
+             dbo.collection("insurance").insert(jsonObj, function(er, res) {
+                   console.log(`1 document inserted. ${jsonObj.length} items  saved so far`);
+                   db.close();
+              });
+            })
+      });
+    });
