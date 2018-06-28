dd82f7f7a8fa59f63dc90381d0d792c86dd2c0cf
julius1932 <30441147+julius1932@users.noreply.github.com>
2018-06-28 10:19:56 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -21,10 +21,6 @@ app.use(bodyParser.urlencoded({extended:false}));
 // initialize cookie-parser to allow us access the cookies stored in the browser.
 app.use(cookieParser());
 
-/*app.set('views',path.join(__dirname,'views'));
-app.engine('handlebars',exhbs({defaultLayout:'main'}));
-app.set('view engine','handlebars');*/
-
 // Routes
 app.get("/",function(req,res){
 	  res.sendFile( __dirname+'/index.html');
@@ -33,20 +29,53 @@ app.get('/insurance',function(req,res){
   //var searchValue =req.body.search;
    var searchValue =req.query.search;
    console.log(searchValue);
+		 regxz='^'+searchValue+'[a z]*';
+		  console.log(regxz);
    if(searchValue){
-     var query= { $text: { $search: searchValue } };
+     var query=  {$text: { $search: regxz}};
      MongoClient.connect(urlll, function(rr, db) {
          if (rr) {isfound=false; return;};
-         //var dbo = db.db("insurance_db");//insurance_db
+          //var dbo = db.db("insurance_db");//insurance_db
          var dbo = db.db("heroku_pv94v0fr");
          /*dbo.createIndex("insur",{ pn:'text', cr:'text',st:'text',yr:'text', pid:'text',mt:'text'},function(err,op) {
            console.log(err);
          });*/
+				 var feilds=['cr','yr','st','mt','pn'];
          dbo.collection("insur").find(query).toArray(function(errr, reslts) {
              if (errr) {throw errr;return;}
-             console.log(reslts.length);
+						 var arr0,arr1,arr2;
+						 arr0=arr1=arr2=[];
+						  reslts.forEach(function(row){
+								var mtched=false;
+								feilds.forEach(function(feild){
+									//console.log(row[feild]);
+									 if(isNaN(row[feild])&&row[feild].startsWith(searchValue)){
+										 mtched=true;
+									 }
+								 });
+								 if(mtched){
+									   arr0.push(row);
+								 }else {
+									  feilds.forEach(function(feild){
+   									 if(isNaN(row[feild])&&row[feild].includes(searchValue)){
+   										 mtched=true;
+   									 }
+   								 });
+									 if(mtched){
+											arr1.push(row);
+									 }else{
+										 if(mtched){
+												arr2.push(row);
+										}
+									 }
+								 }
+
+							 });
+							 arr0=arr0.concat(arr1);
+						   arr0=arr0.concat(arr2);
+             console.log(arr0.length);
              db.close();
-              res.jsonp(reslts);
+              res.jsonp(arr0);
            //  res.render('hom',{results :reslts,num:reslts.length});
          });
        })
